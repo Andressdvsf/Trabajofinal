0f15ff2c5d16330422bd110c09fbf784db3a8930
Andressdvsf <111907814+Andressdvsf@users.noreply.github.com>
2022-11-26 07:25:16 -0500

    Add files via upload

---
+++ directorio/Directorio.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package app.directorio;
+
+import formulario.inicio;
+
+
+/**
+ *
+ * @author Andres
+ */
+public class Directorio {
+
+    public static void main(String[] args) {
+        inicio i = new inicio();
+      i.setVisible(true);
+      
+        
+    }
+}
